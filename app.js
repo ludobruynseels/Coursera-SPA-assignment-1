c19f17514c9f982a071a1c36458be046411350e1
Ludo <Ludo@ludobruynseels.com>
2017-01-14 20:25:26 +0100

    Solution

---
+++ app.js
@@ -0,0 +1,33 @@
+(function () {
+'use strict';
+
+angular.module('lunchChecker', [])
+.controller('lunchCheckerController', lunchCheckerController);
+
+lunchCheckerController.$inject = ['$scope'];
+function lunchCheckerController($scope, $filter) {
+
+  $scope.EvaluateMenu =  function ()
+  {
+      if (!$scope.lunchItemList) {
+        $scope.message = 'Please enter data first';
+      return;
+  }
+      var s = $scope.lunchItemList || '';
+
+      var items = s.split(",");
+      var numItems = items.length;
+
+      switch (numItems) {
+          case 1:
+          case 2:
+          case 3:
+              $scope.message = "Enjoy!";
+              break;
+          default:
+              $scope.message = "Too much!";
+      }
+  };
+  };
+
+})();
